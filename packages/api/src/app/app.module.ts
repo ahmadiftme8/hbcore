c46a916a86f4e996629f39722b7e49d6661db0af
Alireza Jafartash <alirezaopmc@gmail.com>
2025-12-01 18:19:10 +0330

    chore(api): format code and update scripts
    - Format package.json and app.module.ts (tabs to spaces)
    - Rename start:dev to dev and start:debug to debug
    - Remove start:prod script

--- packages/api/src/app/app.module.ts
+++ packages/api/src/app/app.module.ts
@@ -1,12 +1,11 @@
-import { ConfigModule } from "@/config/config.module";
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@/config/config.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-	imports: [ConfigModule],
-	controllers: [AppController],
-	providers: [AppService],
+  imports: [ConfigModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
-
